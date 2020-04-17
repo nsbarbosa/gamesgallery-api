7fad1a20a4903f94f58c775e34aed4cdcd99ec21
Natasha Barbosa <n.natasha.barbosa@gmail.com>
2020-04-17 10:37:41 -0300

    heroku_mysql_connection

--- config/database.php
+++ config/database.php
@@ -22,7 +22,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'heroku_mysql_connection'),
 
     /*
     |--------------------------------------------------------------------------
